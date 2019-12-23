3abc057611cddd41d74db3ca3411d97f2a7bfae5
krable91 <dydansp@naver.com>
2019-12-24 02:21:12 +0900

    js clock part2

--- clock.js
+++ clock.js
@@ -5,11 +5,12 @@ function getTime(){
     const date = new Date();
     const minutes = date.getMinutes();
     const hours = date.getHours();
-    const seconds = date.getSeconds();
-    clockTitle.innerText = `${hours}:${minutes}:${seconds}`;
+    clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}`;
+
 }
 
 function init() {
     getTime();
+    setInterval(getTime, 1000);
 }
 init();
