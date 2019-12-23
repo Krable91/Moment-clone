3a58b8f8ba46e1d94873e3ac823a3700b15082de
krable91 <dydansp@naver.com>
2019-12-24 04:29:56 +0900

    Storage

--- greeting.js
+++ greeting.js
@@ -5,6 +5,23 @@ const form = document.querySelector(".js-form"),
 const USER_LS = "currentUser",
     SHOWING_CN = "showing";
 
+function saveName(text) {
+        localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit (event) {
+    event.preventDefault();
+    const currentValue = input.value;
+    paintGreeting(currentValue);
+    saveName(currentValue);
+}    
+
+
+function askForName() {
+        form.classList.add(SHOWING_CN);
+        form.addEventListener("submit", handleSubmit)
+}
+
 function paintGreeting(text) {
     form.classList.remove(SHOWING_CN);
     greeting.classList.add(SHOWING_CN);
@@ -15,10 +32,11 @@ function paintGreeting(text) {
 function loadName(){
     const currentUser = localStorage.getItem(USER_LS);
     if(currentUser === null){
-            //유저가 있을 때
+        askForName();
+            //유저가  없을 때
     } else {
         paintGreeting(currentUser);
-            //유저가 없을 때
+            //유저가 있을 때
     }
 }
  
